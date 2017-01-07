7773d4b66b98ec5cc4b6150f8dd61625e807f1ba
Pouyan <pouyan@pouyan.local>
2017-01-07 20:33:46 +0330

    fix time , and add registres count

--- report.py
+++ report.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from pymongo import MongoClient
+import jdatetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def report():
     client = MongoClient()
     db = client.get_database('score_bot')
     collec = db.get_collection('sc_dataset')
@@ -14,13 +15,19 @@ def hello_world():
     result += "<table class='responstable'><tr><th >time</th> <th  >firstname</th><th  >lastname</th><th  >username</th><th  >command</th><th  >chat_id</th><th  >chat type</th></tr>"
     client.close()
     for item in contents:
-        result += "<tr><td  >" + str(item['date'])
+        result += "<tr><td  >" + str(jdatetime.datetime.fromtimestamp(item['date']))
         result += "</td><td  >{0} </td> <td  >{1}</td>  <td  >{2}</td>  <td  >{3}</td>  <td  >{4}</td>  <td  >{5}</td> </tr> ".format(
             item['from']['first_name'], item['from']['last_name'], item['from']['username'], item['text'],
             item['chat']['id'], item['chat']['type'])
 
     return result + "</table>"
 
-    
+
+@app.route('/registers')
+def registers():
+    client = MongoClient()
+    db = client.get_database('score_bot')
+    collec = db.get_collection('sc_dataset')
+    return len(collec.distinct("from.id"))
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
